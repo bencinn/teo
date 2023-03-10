ffe3f36039ebbe608fe6c23e3b4be3a5c269aa16
hansnnn <pp.rongpan@gmail.com>
2023-03-10 20:46:54 +0700

    Change argument stuff

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,15 @@ mod program;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(short, long)]
-    file: String,
+    /// Read from <FILE_NAME>
+    #[arg(short, long, default_value = "main.teo")]
+    file_name: String,
+    
 }
 
 fn main() {
     let args = Args::parse();
-    let vec_commands: Vec<Commands> = Commands::read_file(&args.file);
+    let vec_commands: Vec<Commands> = Commands::read_file(&args.file_name);
     let program: Program = Program {
         commands: vec_commands,
         current_line: 0,
